d2d6e3283657599c35411165306b16c60c48ec02
Xybron <ronaldnettey@yahoo.com>
2023-08-20 16:57:48 +0000

    Change to future

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GlobalConfiguration().loadFromAsset();
-  configureDependencies();
+  await configureDependencies();
   runApp(const MyApp());
 }
 
